597710d2c75387688d4ee0b407217af763476149
Vinish102 <112571678+Vinish102@users.noreply.github.com>
2024-08-05 20:12:08 +0530

    Add files via upload

---
+++ 7. Markets by Gross Sales.sql
@@ -0,0 +1,21 @@
+-- top 2 markets in every region by their gross sales amount in FY=2021
+WITH cte1 AS (
+    SELECT
+        c.market,
+        c.region,
+        ROUND(SUM(gross_price_total) / 1000000, 2) AS gross_sales_mln
+    FROM gross_sales s
+    JOIN dim_customer c
+        ON c.customer_code = s.customer_code
+    WHERE fiscal_year = 2021
+    GROUP BY c.market, c.region
+    ORDER BY gross_sales_mln DESC
+),
+cte2 AS (
+    SELECT *,
+        DENSE_RANK() OVER (PARTITION BY region ORDER BY gross_sales_mln DESC) AS drnk
+    FROM cte1
+)
+SELECT * 
+FROM cte2 
+WHERE drnk <= 2;
